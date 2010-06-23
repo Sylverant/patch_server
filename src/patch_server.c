fbbe430ae7160202602c526f491104609d67c8f0
Lawrence Sebald <ljsebald@users.sourceforge.net>
2010-06-23 21:42:54 +0000

    License change, Phase 3. Patch Server is now licensed under the Affero GPL v3.

--- src/patch_server.c
+++ src/patch_server.c
@@ -4,15 +4,15 @@
     Copyright (C) 2009 Lawrence Sebald
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License version 3 as
-    published by the Free Software Foundation.
+    it under the terms of the GNU Affero General Public License version 3
+    as published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
@@ -575,8 +575,6 @@ static void read_welcome_message() {
 
     /* Clean up... */ 
     free(buf);
-
-    debug(DBG_LOG, "Ok (%d bytes)\n", welcome_msg_size);
 }
 
 /* Process one patch packet. */
